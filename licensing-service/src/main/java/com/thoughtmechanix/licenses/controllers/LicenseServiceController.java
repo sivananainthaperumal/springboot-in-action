9da440e031c586c9004c302d5a03c8c30265f8b1
sivan <sivananaintha.perumal@gmail.com>
2023-10-27 10:56:07 -0400

    working change after integrating with spring cloud config and mysql

--- licensing-service/src/main/java/com/thoughtmechanix/licenses/controllers/LicenseServiceController.java
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/controllers/LicenseServiceController.java
@@ -2,7 +2,9 @@ package com.thoughtmechanix.licenses.controllers;
 
 import com.thoughtmechanix.licenses.model.License;
 import com.thoughtmechanix.licenses.services.LicenseService;
+import com.thoughtmechanix.licenses.config.ServiceConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,22 +12,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value="v1/organizations/{organizationId}/licenses")
 public class LicenseServiceController {
     @Autowired
     private LicenseService licenseService;
 
+    @Autowired
+    private ServiceConfig serviceConfig;
+
+    @RequestMapping(value="/",method = RequestMethod.GET)
+    public List<License> getLicenses( @PathVariable("organizationId") String organizationId) {
+        return licenseService.getLicensesByOrg(organizationId);
+    }
+
     @RequestMapping(value="/{licenseId}",method = RequestMethod.GET)
     public License getLicenses( @PathVariable("organizationId") String organizationId,
                                 @PathVariable("licenseId") String licenseId) {
 
-        //return licenseService.getLicense(licenseId);
-        return new License()
-            .withId(licenseId)
-            .withOrganizationId(organizationId)
-            .withProductName("Teleco")
-            .withLicenseType("Seat");
+        return licenseService.getLicense(organizationId,licenseId);
     }
 
     @RequestMapping(value="{licenseId}",method = RequestMethod.PUT)
@@ -33,9 +40,9 @@ public class LicenseServiceController {
         return String.format("This is the put");
     }
 
-    @RequestMapping(value="{licenseId}",method = RequestMethod.POST)
-    public String saveLicenses( @PathVariable("licenseId") String licenseId) {
-        return String.format("This is the post");
+    @RequestMapping(value="/",method = RequestMethod.POST)
+    public void saveLicenses(@RequestBody License license) {
+       licenseService.saveLicense(license);
     }
 
     @RequestMapping(value="{licenseId}",method = RequestMethod.DELETE)
